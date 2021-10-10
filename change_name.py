c76d725d17633e61915df8af1d3e8aaeb0a6330a
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-10-11 00:22:05 +0900

    Update change_name.py
    변수명 변경

--- change_name.py
+++ change_name.py
@@ -4,7 +4,11 @@ import os
 FILE_PATH = 'D:/Downloads/'
 FILE_NAMES = os.listdir(FILE_PATH)
 for now_name in FILE_NAMES:
-    change_names = now_name.split(' ', 1)
-    change_name = change_names[1].replace('[', '(').replace(']', ')')
-    os.rename(os.path.join(FILE_PATH, now_name), os.path.join(FILE_PATH, change_name))
+    # 바꿀 이름에 대한 처리
+    change_name = now_name
+    # 경로 + 파일명
+    now_file = os.path.join(FILE_PATH, now_name)
+    change_file = os.path.join(FILE_PATH, change_name)
+    # 파일에 대한 이름 변경
+    os.rename(now_file, change_file)
     print(change_name)

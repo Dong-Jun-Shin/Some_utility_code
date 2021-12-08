3c2952a77315a39327fe8672d4c60861ca1d5eb1
Dongjun-Shin <tlsehdwns239@gmail.com>
2021-12-08 10:30:09 +0900

    Update change_name.py
    - 폴더 내 파일에 대한 처리 > 폴더의 하위 폴더까지 모두 탐색하도록 수정

--- change_name.py
+++ change_name.py
@@ -1,14 +1,16 @@
-# 이름 변경
+# 현재 디렉토리의 하위 파일들 이름 변경
 import os
 
 FILE_PATH = 'D:/Downloads/'
-FILE_NAMES = os.listdir(FILE_PATH)
+FILE_NAMES = os.walk(FILE_PATH)
+# now_name : [0]현재 경로, [1]현재 경로의 폴더, [2]현재 경로의 파일
 for now_name in FILE_NAMES:
-    # 바꿀 이름에 대한 처리
-    change_name = now_name
-    # 경로 + 파일명
-    now_file = os.path.join(FILE_PATH, now_name)
-    change_file = os.path.join(FILE_PATH, change_name)
-    # 파일에 대한 이름 변경
-    os.rename(now_file, change_file)
-    print(change_name)
+    for file in now_name[2]:
+        # 바꿀 이름에 대한 처리
+        change_name = file.replace('바꿀 글자', '바뀐 후 글자')
+        # 경로 + 파일명
+        now_file = os.path.join(now_name[0], file)
+        change_file = os.path.join(now_name[0], change_name)
+        # 파일에 대한 이름 변경
+        os.rename(now_file, change_file)
+        print(change_name)
